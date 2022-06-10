5a37bc4d73b7f450f52e4879fcbc8339009d6be5
Chris Kaltwasser <chris@chrisk.com>
2022-06-10 18:13:51 -0400

    two types of ChaCha

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,37 @@ fn main() {
     let n1: u8 = srng.gen();
     let n2: u16 = srng.gen();
 
-    println!("Fixed Seeded Random u8: {}", n1);
-    println!("Fixed Seeded Random u16: {}", n2);
-    println!("Fixed Seeded Random u32: {}", srng.gen::<u32>());
-    println!("Fixed Seeded Random i32: {}", srng.gen::<i32>());
-    println!("Fixed Seeded Random float: {}\n\n", srng.gen::<f64>());
+    println!("ChaCha8 Fixed Seeded Random u8: {}", n1);
+    println!("ChaCha8 Fixed Seeded Random u16: {}", n2);
+    println!("ChaCha8 Fixed Seeded Random u32: {}", srng.gen::<u32>());
+    println!("ChaCha8 Fixed Seeded Random i32: {}", srng.gen::<i32>());
+    println!("ChaCha8 Fixed Seeded Random float: {}\n\n", srng.gen::<f64>());
+
+    let mut srng = ChaCha8Rng::from_entropy();
+    let n1: u8 = srng.gen();
+    let n2: u16 = srng.gen();
+    println!("ChaCha8 Seeded From Entropy Random u8: {}", n1);
+    println!("ChaCha8 Seeded From Entropy Random u16: {}", n2);
+    println!("ChaCha8 Seeded From Entropy Random u32: {}", srng.gen::<u32>());
+    println!("ChaCha8 Seeded From Entropy Random i32: {}", srng.gen::<i32>());
+    println!("ChaCha8 Seeded From Entropy Random float: {}\n\n", srng.gen::<f64>());
+
+    let mut srng = ChaCha20Rng::seed_from_u64(2);
+    let n1: u8 = srng.gen();
+    let n2: u16 = srng.gen();
+
+    println!("ChaCha20 Fixed Seeded Random u8: {}", n1);
+    println!("ChaCha20 Fixed Seeded Random u16: {}", n2);
+    println!("ChaCha20 Fixed Seeded Random u32: {}", srng.gen::<u32>());
+    println!("ChaCha20 Fixed Seeded Random i32: {}", srng.gen::<i32>());
+    println!("ChaCha20 Fixed Seeded Random float: {}\n\n", srng.gen::<f64>());
 
     let mut srng = ChaCha20Rng::from_entropy();
     let n1: u8 = srng.gen();
     let n2: u16 = srng.gen();
-    println!("Seeded From Entropy Random u8: {}", n1);
-    println!("Seeded From Entropy Random u16: {}", n2);
-    println!("Seeded From Entropy Random u32: {}", srng.gen::<u32>());
-    println!("Seeded From Entropy Random i32: {}", srng.gen::<i32>());
-    println!("Seeded From Entropy Random float: {}", srng.gen::<f64>());
+    println!("ChaCha20 Seeded From Entropy Random u8: {}", n1);
+    println!("ChaCha20 Seeded From Entropy Random u16: {}", n2);
+    println!("ChaCha20 Seeded From Entropy Random u32: {}", srng.gen::<u32>());
+    println!("ChaCha20 Seeded From Entropy Random i32: {}", srng.gen::<i32>());
+    println!("ChaCha20 Seeded From Entropy Random float: {}", srng.gen::<f64>());
 }
